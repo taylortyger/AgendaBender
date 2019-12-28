a4594a1f54a2e44fd56ad127ca15bdcfac82b020
Taylor Tyger <taylortyger@gmail.com>
2019-12-27 18:00:46 -0700

    Lay out structure of the Task object

---
+++ data/Task.js
@@ -0,0 +1,10 @@
+class Task {
+    constructor(id, title, course, deadline = null, completed = false, scheduledDate = null) {
+        this.id = id;
+        this.title = title;
+        this.course = course;
+        this.deadline = deadline;
+        this.completed = completed;
+        this.scheduledDate = scheduledDate;
+    }
+}
