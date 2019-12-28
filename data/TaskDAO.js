cfc9c4e2a7bac53199c08709d0ab536e4e0ad6e8
Taylor Tyger <taylortyger@gmail.com>
2019-12-28 16:01:06 -0700

    Add basic query functionality to TaskDAO

--- data/TaskDAO.js
+++ data/TaskDAO.js
@@ -2,4 +2,26 @@ class TaskDAO {
     constructor() {
         this.tasks = [];
     }
+
+    find(criteria) {
+        if(Object.keys(criteria).length === 0) return this.tasks;
+        else {
+            return this.tasks.filter(task => this.matchesCriteria(task, criteria));
+        }
+    }
+
+    findById(id) {
+        return this.tasks.filter(task => task.id == id);
+    }
+
+    //meant to be private
+    matchesCriteria(task, criteria) {
+        matches = true;
+        for(key in criteria) {
+            if (criteria[key] != task[key]){
+                matches = false;
+            }
+        }
+        return matches;
+    }
 }
