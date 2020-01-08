3f04bfc971e9656c8ab10e6b18012c11a1a51043
Taylor Tyger <taylortyger@gmail.com>
2020-01-07 18:21:27 -0700

    Layout test cases for TaskRepository

--- data/TaskRepository.test.js
+++ data/TaskRepository.test.js
@@ -10,21 +10,48 @@ const TaskRepository = require('./TaskRepository');
 
 describe('TaskRepository', () => {
   describe('getAll()', () => {
-
+    it('should return an empty array if no tasks exist', () => {});
+    it('should return an array of all tasks', () => {});
   });
   describe('getById()', () => {
-
+    it('should return the task with the given id', () => {});
+    it('should return null if a task with the given id does not exist', () => {});
   });
   describe('update()', () => {
+    it('should return the updated task', () => {});
+    it('should update any provided valid fields', () => {});
+    it('should return null if update is unsuccessful (no props object, no id field, task does not exist)', () => {
 
+    });
   });
   describe('getByCourse()', () => {
+    it('should return an array of all tasks matching the provided course', () => {
 
+    });
+    it('should return an empty array if no tasks match the course', () => {
+
+    });
   });
   describe('newTask()', () => {
+    it('should return the created task', () => {
 
+    });
+    it('should add the new task to the DAO', () => {
+
+    });
+    it('should return null if task creation is unsuccessful', () => {
+
+    });
   });
   describe('deleteById()', () => {
+    it('should return the deleted task', () => {
 
+    });
+    it('should remove the task with the given id from the DAO', () => {
+
+    });
+    it('should return null if task deletion is unsuccessful (task not found, id not provided)', () => {
+
+    });
   });
 });
